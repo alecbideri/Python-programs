c630d8e438dd259668fa1628aa9b3ad1b46b001d
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2024-04-05 09:52:54 +0200

    Delete QUIZ_GAME_PART2 directory

--- QUIZ_GAME_PART2/question_model.py
+++
@@ -1,5 +0,0 @@
-class question:
-    def __init__(self, q_text, q_answer):
-        self.text = q_text
-        self.answer = q_answer
-
--- QUIZ_GAME_PART2/quiz_brain.py
+++
@@ -1,6 +0,0 @@
-from data import question_data
-from question_model import question
-
-
-def get_questiona():
-    return (question(item["text"], item["answer"]) for item in question_data)
